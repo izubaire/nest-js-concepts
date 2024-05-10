00183f39b330afe4a8a6784678510c3bdc2ee770
zubair <m.zobaire@gmail.com>
2024-05-10 05:02:20 +0500

    recieve entitities with their relation

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -12,11 +12,11 @@ export class CoffeesService {
   ) {}
 
   findAll() {
-    return this.coffeeRepository.find();
+    return this.coffeeRepository.find({ relations: ['flavors'] });
   }
 
   async findOne(id: string) {
-    const coffee = await this.coffeeRepository.findOne({ where: {id: parseInt(id)} });
+    const coffee = await this.coffeeRepository.findOne({ where: {id: parseInt(id)}, relations: ['flavors'] });
     if (!coffee) {
       throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
     }
